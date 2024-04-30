ceb6ce4dcae27d27ecfa41a344217ca17b0e6241
Kimberly Maddox <160989452+kmaddox1@users.noreply.github.com>
2024-04-30 23:09:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// new Line
+//console.log('K'); you can put them all on the same line
+//console.log('I');
+//console.log('M'); 
+
+console.log('K\nI\nM\nB\nE\nR\nL\nY');
+
+// single quotes + apostrophes
+console.log( 'That\'s so fetch!');
+
+// quotes within strings
+
+// if inside and outside quotes match in format, all is well
+console.log("In William Shakespear's Hamlet, it was 'to be, or not to be, that is the question'");
+
+// if inner and outer quotes are the same format, need to escape the inner quotes
+console.log("In the 21st century, \"to code, or not to code, that is the challenge.\"");
+
+
+console.log(`Fancy ES6 strings with backticks.
+This is the second line of the quote
+Let's type a line with an apostrophe in it.`);
